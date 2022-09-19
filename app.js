233125205ae893773cdc999444e97b4d55ee16c0
LoisKOUNINEF <lkouninef@gmail.com>
2022-09-19 11:30:26 +0200

    first exercise

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
+let pokemons = require('./mock-pokemon');
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello, world !'));
 
+app.get('/api/pokemons', (req, res) => {
+  res.send(`There are a total of ${pokemons.length} Pokemons, for now.`)
+})
+
 app.get('/api/pokemons/:id', (req, res) => {
-  const id = req.params.id
-  res.send(`Here comes Pokemon number ${id}`)
+  const id = parseInt(req.params.id)
+  const pokemon = pokemons.find(pokemon => pokemon.id === id)
+  res.send(`Here comes ${pokemon.name}`)
 })
 
 app.listen(port, () => console.log(`Our NodeJS API is running on http://localhost:${port}`));
