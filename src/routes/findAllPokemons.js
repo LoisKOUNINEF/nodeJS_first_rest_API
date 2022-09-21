33688ee5a6b3dff4c52ede9c26e27b27a28fca6b
LoisKOUNINEF <lkouninef@gmail.com>
2022-09-21 17:17:10 +0200

    advanced requests : search by name (include). Order and limit response.

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,7 +1,32 @@
-const { Pokemon } = require('../db/sequelize')
+const { Pokemon } = require('../db/sequelize');
+const { Op } = require ('sequelize');
 
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
+    if(req.query.name) {
+      const name = req.query.name
+      const limit = parseInt(req.query.limit) || 5
+
+      if(name.length <= 2) {
+        const message = 'Please enter at least 3 characters in search field.'
+        return res.status(400).json({ message })
+      }
+
+      return Pokemon.findAndCountAll({
+        where: {
+          name: {
+            [Op.like]: `%${name}%`
+          }
+        },
+        limit: limit,
+        order: ['name']
+        // order: [['name', 'DESC']]
+      })
+      .then(({count, rows}) => {
+        const message = `There are ${count} that contain ${name}.`
+        res.json({ message, data: rows })
+      })
+    } else {
     Pokemon.findAll()
       .then(pokemons => {
         const message = 'Pokemons list fetched !'
@@ -11,5 +36,6 @@ module.exports = (app) => {
         const message = 'Cannot fetch Pokemons list. Please try again in a moment.'
         res.status(500).json({ message, data: error })
       })
+    }
   })
 }
