c0a7a8a460e8d2965327abe662e0cd0f1c252378
LoisKOUNINEF <lkouninef@gmail.com>
2022-09-22 11:33:59 +0200

    remove morgan

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -4,20 +4,33 @@ const UserModel = require('../models/user');
 const pokemons = require('./mock-pokemon');
 const bcrypt = require('bcrypt');
 
-const sequelize = new Sequelize('pokedex', 'root', '', {
-  host: 'localhost',
-  dialect: 'mariadb',
-  dialectOptions: {
-    timezone: 'Etc/GMT-2',
-  },
-  logging: false
-})
+let sequelize
+
+if(process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize('databasename', 'username', 'password', {
+    host: 'host',
+    dialect: 'mariadb',
+    dialectOptions: {
+      timezone: 'Etc/GMT-2',
+    },
+    logging: true
+  })
+} else {
+  sequelize = new Sequelize('pokedex', 'root', '', {
+    host: 'localhost',
+    dialect: 'mariadb',
+    dialectOptions: {
+      timezone: 'Etc/GMT-2',
+    },
+    logging: false
+  })
+}
 
 const Pokemon = PokemonModel(sequelize, DataTypes)
 const User = UserModel(sequelize, DataTypes)
 
 const initDb = () => {
-  return sequelize.sync({force: true}).then(_ => {
+  return sequelize.sync().then(_ => {
     pokemons.map(pokemon => {
       Pokemon.create({
         name: pokemon.name,
@@ -31,8 +44,8 @@ const initDb = () => {
     bcrypt.hash('123456', 10)
     .then(hash => {
       User.create({
-      username: 'Lolo',
-      password: hash
+        username: 'Lolo',
+        password: hash
       }).then(user => console.log(user.toJSON()))
     })
 
